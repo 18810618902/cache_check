15a151521c9c2afa7a960ab5e6f377da7b3905af
Mr.right <18810618902@163.com>
2017-12-25 12:06:15 +0800

    Update oui_api_info.py

--- cache_check/conf/oui_api_info.py
+++ cache_check/conf/oui_api_info.py
@@ -5,17 +5,17 @@ usename_pw = '
 #=========【1】=========
 #offline=0    去除 offline和broken的机器,通过band api查询ip时，没有判断offline的参数，需要结合这个表
 
-node_list = 'https://pantherapi.cdnetworks.com/rest/int/' + usename_pw +'/node/list/'
+node_list = 'https://pantherapi/rest/int/' + usename_pw +'/node/list/'
 
 #============【2】==================
 #这个获取的band包括offline的，需要注意下
 #可以获取都某个dns_prefix下面有多少band,(band_id)
 
-edge_info = 'https://pantherapi.cdnetworks.com/rest/int/' + usename_pw + '/get_edge_info/'
+edge_info = 'https://pantherapi/rest/int/' + usename_pw + '/get_edge_info/'
 
 
 #可以知道这个band下面配置了哪些vip，从何得到对应prefix下面有哪些ip
-band_vip = 'https://pantherapi.cdnetworks.com/rest/int/' + usename_pw + '/get_band_vip/'
+band_vip = 'https://pantherapi/rest/int/' + usename_pw + '/get_band_vip/'
 
 
 #【1】与【2】 量表联合查询排除掉offline的机器
@@ -26,7 +26,7 @@ band_vip = 'https://pantherapi.cdnetworks.com/rest/int/' + usename_pw + '/get_ba
 #========[3]===========
 #获取prefix
 
-service_name = 'https://pantherapi.cdnetworks.com/rest/int/' + usename_pw + '/service/list/'
+service_name = 'https://pantherapi/rest/int/' + usename_pw + '/service/list/'
 
 
 #========[4]============
